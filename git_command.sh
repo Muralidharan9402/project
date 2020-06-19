580af9ef72cef21793858eb0207cc07e59264a95
root <root@instance-2.us-central1-a.c.central-node-258914.internal>
2020-06-19 02:34:15 +0000

    NewCommit

--- git_command.sh
+++ git_command.sh
@@ -1,9 +1,9 @@
-read "Enter the local repository directory:" directory
+read -p "Enter the local repository directory:" directory
 chdir $directory #/etc/ansible/project
 git add --all
-read "Enter the URL to add project to remote repository:" remotePath
+read -p "Enter the URL to add project to remote repository:" remotePath
 git remote add MovieMax $remotePath  #https://github.com/Muralidharan9402/test_unix_push.git
-read "Enter the Commit message:" commitMessage
+read -p "Enter the Commit message:" commitMessage
 git commit -am $commitMessage #"Regular auto-commit "
 #git pull origin master
 #git rebase master
